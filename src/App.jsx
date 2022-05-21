d5692eeef2363e9ffade69e7d7cd76d735668b33
kknyapple <kknyapple@gmail.com>
2022-05-21 21:27:33 +0900

    route로 detail 페이지 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import bg from "./bg.png";
 import data from "./data.jsx";
+import Detail from "./routes/Detail.jsx";
 
 function App() {
   const [shoes, setShoes] = useState(data);
@@ -11,7 +13,7 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand>Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -19,8 +21,23 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
-      <Cards shoes={shoes} />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: `url(${bg})` }}
+              ></div>
+              <Cards shoes={shoes} />
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+      </Routes>
     </>
   );
 }

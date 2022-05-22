ba9007fefa132f1838db70c03486ef73219708a8
kknyapple <kknyapple@gmail.com>
2022-05-23 01:58:51 +0900

    Lifecycle과 useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import "./App.css";
--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
   const { id } = useParams();
+  const [disappear, setDisappear] = useState(0);
+  const [num, setNum] = useState("");
   const findProduct = props.shoes.find((x) => x.id === parseInt(id));
+
+  useEffect(() => {
+    const a = setTimeout(() => {
+      setDisappear(1);
+    }, 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      alert("숫자를 입력하시오");
+    }
+  }, [num]);
+
   return (
     <>
       <div className="container">
         <div className="row">
           <div className="col-md-6">
+            {disappear === 0 ? (
+              <h5 style={{ textAlign: "center", margin: 20 }}>
+                2초 후에 사라질 예정
+              </h5>
+            ) : null}
+
             <img
               src={`https://codingapple1.github.io/shop/shoes${
                 parseInt(id) + 1
@@ -24,6 +47,12 @@ function Detail(props) {
           </div>
         </div>
       </div>
+      <hr></hr>
+      <input
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      ></input>
     </>
   );
 }
